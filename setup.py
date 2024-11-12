efb0b6876a0b654e439bc23798e1c3bb6a348a7a
Sebastien Chapeland <43520603+SebastienChapeland@users.noreply.github.com>
2024-11-12 23:29:45 +0100

    change setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
  
-setup(name = "library", packages = find_packages())
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name = "library", 
+    packages = find_namespace_packages(),
+    install_requires=requirements,
+)
